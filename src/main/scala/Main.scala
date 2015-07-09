90574f5e2166127e8c05587c74777ae73b37a9bb
Giymo11 <gizmo.head@yahoo.de>
2015-07-09 14:46:49 +0200

    Refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.concurrent.atomic.AtomicInteger
 
+import scala.StringBuilder
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future, Promise}
 import scala.io.{BufferedSource, StdIn}
@@ -21,120 +22,100 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     import scala.concurrent.duration._
+    
+    val isReport = args.length != 0 && args(0) == "-p"
 
-    val ips = if(args.length == 0) {
+    val ips = if(isReport || args.length == 0) {
       print("Please enter the IP: ")
       Seq(StdIn.readLine())
     } else {
      args.toSeq
     }
 
-    def tryIp(ip: String): Future[Socket] = {
-      def getSocketForPort(port: Int): Future[Socket] = Future {
-        val socket = new Socket()
-        val addr = new InetSocketAddress(ip, port)
-        try {
-          socket.connect(addr, networkingTimeout)
-        } catch {
-          case ex: Exception => throw new Exception(addr.getAddress.toString)
-        }
-        socket
-      }
-
-      val promise = Promise[Socket]()
-
-      var count = new AtomicInteger(ports.size)
-
-      ports.map(getSocketForPort).foreach(_ onComplete {
-        case Success(socket) => promise.trySuccess(socket)
-        case Failure(ex) => if(count.decrementAndGet() == 0) promise.failure(ex)
-      })
-
-      promise.future
-    }
-
     val sockets = ips.map(tryIp).map(_.map { socket =>
-      goForIt(Analyzer(socket))
-      socket.close()
+      val analyzer = Analyzer(socket)
+      analyzer.goForIt()
+
+      val file = getFile("./" + analyzer.modelNr, s"${{analyzer.serial}} - ${{analyzer.model}}.txt")
+
+      val in = new BufferedSource(new FileInputStream(file))
+      val oldContent = in.getLines().toList
+      in.close()
+
+      val builder = new StringBuilder(oldContent.size * 2)
+
+      val linebreak = System.lineSeparator()
+      builder.append("Date: " + analyzer.date).append(linebreak)
+      builder.append(s"Model: ${{analyzer.model}}, Serial: ${{analyzer.serial}}").append(linebreak * 2)
+      builder.append(prettyFormat(analyzer.config.get)).append(linebreak)
+      builder.append(prettyFormat(analyzer.tlist.get)).append(linebreak)
+      builder.append(prettyFormat(analyzer.vlist.get)).append(linebreak * 3)
+      builder.append(oldContent.mkString(linebreak))
+
+      val out = new FileOutputStream(file, false) // important
+      out.write(builder.mkString.getBytes)
+      out.close()
+
       socket.getInetAddress
     })
 
     val tries = Await.result(Future.sequence(sockets.map(future2try)), Duration.Inf)
     tries.filter(_ isSuccess).foreach(addr =>  println(s"${{addr.get.toString}} worked"))
-    tries.filter(_ isFailure).foreach(addr =>  println(s"${{addr.failed.get.getMessage}} didn't work"))
+    tries.filter(_ isFailure).foreach(addr =>  {
+      addr.failed.get.printStackTrace()
+      println(s"${{addr.failed.get.getMessage}} didn't work")
+    })
 
     System.exit(0)
   }
 
-  def goForIt(analyzer: Analyzer) = {
-
-    analyzer.sendCommand("v config")
-    analyzer.sendCommand("t list")
-    analyzer.sendCommand("v list!")
-
-    val responseLines = analyzer.readResponses()
-
-    val (config, rest) = responseLines.span(_.startsWith("V")) // important
-    val (tlist, vlist) = rest.span(_.startsWith("T"))
-    val responses = Seq(config.toList, tlist.toList, vlist.toList)
-
-    // [ ]+ matches any number of spaces
-    val pairsList = formatToPairs(responses)
-
-    def isDigit(char: Char): Boolean = {
-      char >= '0' && char <= '9'
+  def tryIp(ip: String): Future[Socket] = {
+    def getSocketForPort(port: Int): Future[Socket] = Future {
+      val socket = new Socket()
+      val addr = new InetSocketAddress(ip, port)
+      try {
+        socket.connect(addr, networkingTimeout)
+      } catch {
+        case ex: Exception => throw new Exception(addr.getAddress.toString)
+      }
+      socket
     }
 
-    val model = pairsList(0).find(pair => pair._1 == "CONFIG[0]").map(_._2).get
-    val modelNr = model.dropWhile(!isDigit(_)).takeWhile(isDigit)
-    val serial = pairsList(2).find(pair => pair._1 == "SERIAL_NUMBER").map(_._2).get.replace(model.split(" ")(0), "").replaceAll("\"|\\s|-", "").dropWhile(_ == '0')
-    val date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime)
+    val promise = Promise[Socket]()
 
-    val dir = new File("./" + modelNr)
+    val count = new AtomicInteger(ports.size)
+
+    ports.map(getSocketForPort).foreach(_ onComplete {
+      case Success(socket) => promise.trySuccess(socket)
+      case Failure(ex) => if(count.decrementAndGet() == 0) promise.failure(ex)
+    })
+
+    promise.future
+  }
+
+  def getFile(dirName: String, fileName: String) = {
+    val dir = new File(dirName)
     if(!dir.exists())
       dir.mkdir()
 
-    val file = new File(dir, s"$serial - $model.txt")
+    val file = new File(dir, fileName)
 
     if(!file.exists())
       file.createNewFile()
 
-    val in = new BufferedSource(new FileInputStream(file))
-    val oldContent = in.getLines().toList
-    in.close()
-
-    val out = new PrintStream(new FileOutputStream(file, false)) // important
-
-    out.println("Date: " + date)
-    out.println(s"Model: $model, Serial: $serial")
-    out.println()
-    out.println(prettyFormat(pairsList))
-    out.println()
-
-    oldContent.foreach(out.println)
-
-    out.close()
+    file
   }
 
-  def formatToPairs(responses: Seq[Seq[String]]): Seq[Seq[(String, String)]] = {
-    responses.map(response => response.map(_.split("[ ]+").drop(3).mkString(" ")).map(param => {
-      val split = param.split("=")
-      split(0) -> split(1)
-    }))
-  }
-
-  def prettyFormat(pairsList: Seq[Seq[(String, String)]]): String = {
+  def prettyFormat(pairs: Seq[(String, String)]): String = {
     val builder = new StringBuilder()
-    pairsList.foreach(pairs => {
-      
-      val lengthInTabs = pairs.maxBy(pair => pair._1.length)._1.length / tabInSpaces + 1
-      
-      pairs.foreach(pair => {
-        val tabs = lengthInTabs - pair._1.length / tabInSpaces
-        builder.append(pair._1).append("\t" * tabs).append(pair._2).append(System.lineSeparator())
-      })
-      builder.append(System.lineSeparator()) // important
+
+    val lengthInTabs = pairs.maxBy(pair => pair._1.length)._1.length / tabInSpaces + 1
+
+    pairs.foreach(pair => {
+      val tabs = lengthInTabs - pair._1.length / tabInSpaces
+      builder.append(pair._1).append("\t" * tabs).append(pair._2).append(System.lineSeparator())
     })
+
     builder.mkString
   }
 
@@ -150,6 +131,9 @@ case class Analyzer(socket: Socket) {
   val out = new PrintStream(socket.getOutputStream)
   lazy val in = new BufferedSource(socket.getInputStream)
 
+  var model, modelNr, serial, date = ""
+  var config, tlist, vlist : Option[Seq[(String, String)]] = None
+
   def sendCommand(command: String): Unit = {
     out.println(command)
     out.flush()
@@ -164,5 +148,33 @@ case class Analyzer(socket: Socket) {
       case ex: SocketTimeoutException => //left blank intentionally
     }
     new BufferedSource(new ByteArrayInputStream(builder.mkString.getBytes)).getLines()
+  }
+
+  def goForIt() = {
+    sendCommand("v config")
+    sendCommand("t list")
+    sendCommand("v list!")
+
+    val responseLines = readResponses()
+
+    socket.close()
+
+    val (configString, rest) = responseLines.span(_.startsWith("V")) // important
+    val (tlistString, vlistString) = rest.span(_.startsWith("T"))
+
+    this.config = Some(configString.toSeq.map(formatToPair))
+    this.tlist = Some(tlistString.toSeq.map(formatToPair))
+    this.vlist = Some(vlistString.toSeq.map(formatToPair))
+
+    model = config.get.find(pair => pair._1 == "CONFIG[0]").map(_._2).get
+    modelNr = model.dropWhile(!_.isDigit).takeWhile(_.isDigit)
+    serial = vlist.get.find(pair => pair._1 == "SERIAL_NUMBER").map(_._2).get.replace(model.split(" ")(0), "").replaceAll("\"|\\s|-", "").dropWhile(_ == '0')
+    date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime)
+  }
+
+  def formatToPair(pair: String): (String, String) = {
+    // [ ]+ matches any number of spaces
+    val parts = pair.split("[ ]+").drop(3).mkString(" ").split("=")
+    parts(0) -> parts(1)
   }
 }
